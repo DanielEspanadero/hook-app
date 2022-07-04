0f1cd5f85f3997046c47078284f31bef51ff9ed8
Daniel Españadero <developer@daniel-espanadero.com>
2022-07-04 22:42:44 +0200

    Memo component created

--- src/HooksApp.jsx
+++ src/HooksApp.jsx
@@ -3,11 +3,12 @@
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 // import { FocusScreen } from './04-useRef/FocusScreen';
-import { Layout } from './05-useLayoutEffect/Layout';
+// import { Layout } from './05-useLayoutEffect/Layout';
+import { Memorize } from './06-memos/Memorize';
 // import { SimpleForm } from './02-useEffect/SimpleForm';
 
 export const HooksApp = () => {
   return (
-    <Layout />
+    <Memorize />
   )
 }
